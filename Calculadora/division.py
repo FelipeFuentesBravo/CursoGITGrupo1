e7f29ab156105a62e538690b6ae21f042e41509e
hek23 <hector.fuentealba@usach.cl>
2016-08-27 11:14:28 -0300

    holi

--- Calculadora/division.py
+++
@@ -1,8 +0,0 @@
-# Funcion para dividir dos numeros float #
-# Entrada: Dos enteros reales            #
-# Salida: Entero                         #
-##########################################
-
-def dividir(x,y):
-  resultado = (float(x)/float(y))
-  return resultado
