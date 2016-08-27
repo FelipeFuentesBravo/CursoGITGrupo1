a709ff3de38d681f5a86c11904e90f5e49a48f95
fokthepencil <f.caraneda@gmail.com>
2016-08-27 11:16:42 -0300

    Removida la carpeta calculadora

---
+++ division.py
@@ -0,0 +1,8 @@
+# Funcion para dividir dos numeros float #
+# Entrada: Dos enteros reales            #
+# Salida: Entero                         #
+##########################################
+
+def dividir(x,y):
+  resultado = (float(x)/float(y))
+  return resultado
