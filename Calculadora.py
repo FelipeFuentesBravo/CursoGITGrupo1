dd30e9e21ef89147d7392b421f2374a328cf3b27
sebakimbo <sebastian.pinto.g@usach.cl>
2016-08-27 11:43:09 -0300

    Calculadora
    aca se junta todo :P

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+import Suma.py
+import Resta.py
+import Multiplicacion.py
+import division.py
+import Potencia.py
+
+
+
+
+
+#Menu
+print " Seleccione una de las siuiente opciones"
+print " 1.Sumar"
+print " 2.Restar"
+print " 3.Multiplicacion"
+print " 4.Division"
+print " 5.Potencia"
+print "-----------------------------------------"
+
+seleccion= ("ingresa el numero de la opcion que quieres: ")
+
