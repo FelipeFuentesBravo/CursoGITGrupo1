a133dbac24bece672d2b8c56ca5e9f0b6e9c6a0f
hek23 <hector.fuentealba@usach.cl>
2016-08-29 20:02:21 -0300

    Error en Potencia

--- Potencia.py
+++ Potencia.py
@@ -1,6 +1,9 @@
 def potencia (base, exponente):
-	resultado = base
+	resultado = 1.0
 	while (exponente > 0):
 		resultado = resultado * base
 		exponente = exponente - 1
+	while (exponente < 0):
+		resultado = resultado / base
+		exponente = exponente + 1
 	return resultado
